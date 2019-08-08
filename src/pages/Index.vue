9481619d1d346f0b5e7032e479cff77def7f0a83
Shivam Mishra <scm.mymail@gmail.com>
2019-08-08 21:04:31 +0530

    feat: now

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -48,7 +48,7 @@ export default {
     PostCard
   },
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Shivam Mishra'
   }
 }
 </script>
--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -42,7 +42,7 @@ export default {
     PostCard
   },
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Shivam Mishra'
   }
 }
 </script>
